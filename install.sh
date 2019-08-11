ab9abd6aef9090e8b4a099b483c7966396ce0e6f
helplessheadless <51339582+helplessheadless@users.noreply.github.com>
2019-08-11 17:10:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ download_files()
     	touch $DIR/log.txt
   	echo "Downloading files from https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive ..."
    	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/dns-test.sh -O $DIR/dns-test.sh && chmod +x $DIR/dns-test.sh
- 	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/wwan-keep-alive.sh -O $DIR/internet-keep-alive.sh && chmod +x $DIR/internet-keep-alive.sh
+ 	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/wwan-keep-alive.sh -O $DIR/wwan-keep-alive.sh && chmod +x $DIR/wwan-keep-alive.sh
     	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/restart-interface.sh -O $DIR/restart-interface.sh && chmod +x $DIR/restart-interface.sh
 	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/restart-router.sh -O $DIR/restart-router.sh && chmod +x $DIR/restart-router.sh
 	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/wwankeepalive -O /etc/init.d/wwankeepalive && chmod +x /etc/init.d/wwankeepalive
