d978de31333f440cf986e3b061793c3011872069
r3yr3 <62443942+r3yr3@users.noreply.github.com>
2022-01-26 02:40:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,12 +31,12 @@ download_files()
 	DIR=/usr/wwan-keep-alive
 	mkdir $DIR
     	touch $DIR/log.txt
-  	echo "Downloading files from https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive ..."
-   	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/dns-test.sh -O $DIR/dns-test.sh && chmod +x $DIR/dns-test.sh
- 	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/wwan-keep-alive.sh -O $DIR/wwan-keep-alive.sh && chmod +x $DIR/wwan-keep-alive.sh
-    	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/restart-interface.sh -O $DIR/restart-interface.sh && chmod +x $DIR/restart-interface.sh
-	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/restart-router.sh -O $DIR/restart-router.sh && chmod +x $DIR/restart-router.sh
-	wget -q --no-check-certificate https://raw.githubusercontent.com/helplessheadless/openwrt-lte-keep-alive/master/wwankeepalive -O /etc/init.d/wwankeepalive && chmod +x /etc/init.d/wwankeepalive
+  	echo "Downloading files from https://raw.githubusercontent.com/r3yr3/openwrt-wwan-keep-alive ..."
+   	wget -q --no-check-certificate https://raw.githubusercontent.com/r3yr3/openwrt-wwan-keep-alive/master/dns-test.sh -O $DIR/dns-test.sh && chmod +x $DIR/dns-test.sh
+ 	wget -q --no-check-certificate https://raw.githubusercontent.com/r3yr3/openwrt-wwan-keep-alive/master/wwan-keep-alive.sh -O $DIR/wwan-keep-alive.sh && chmod +x $DIR/wwan-keep-alive.sh
+    	wget -q --no-check-certificate https://raw.githubusercontent.com/r3yr3/openwrt-wwan-keep-alive/master/restart-interface.sh -O $DIR/restart-interface.sh && chmod +x $DIR/restart-interface.sh
+	wget -q --no-check-certificate https://raw.githubusercontent.com/r3yr3/openwrt-wwan-keep-alive/restart-router.sh -O $DIR/restart-router.sh && chmod +x $DIR/restart-router.sh
+	wget -q --no-check-certificate https://raw.githubusercontent.com/r3yr3/openwrt-wwan-keep-alive/master/wwankeepalive -O /etc/init.d/wwankeepalive && chmod +x /etc/init.d/wwankeepalive
     	finish
 }
 
