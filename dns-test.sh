5edcb7a0e20a954e08db2681a900ba0a826e7208
r3yr3 <62443942+r3yr3@users.noreply.github.com>
2022-01-26 13:43:57 +0800

    Update dns-test.sh

--- dns-test.sh
+++ dns-test.sh
@@ -2,14 +2,14 @@
 # This file is responsible for DNS check. The return value of its process
 # determines the ONLINE/OFFLINE state.
 
-IP_TO_PING=8.8.8.8
-PACKET_COUNT=5
+IP_TO_PING=www.google.com
+PACKET_COUNT=4
 
 ONLINE=0
 
 for i in `seq 1 $PACKET_COUNT`;
         do
-                nc -G 2 -z $IP_TO_PING 53
+                nc -G 2 -z $IP_TO_PING 80
                 RETVAL=$?
 				if [ $RETVAL -eq 0 ]; then
 					ONLINE=1
